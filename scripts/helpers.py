8cb071579a0772115ff6a3be0aadd203fdf4a0c1
socd06 <socd06@gmail.com>
2022-04-05 23:35:09 -0400

    refactor functions into helpers.py

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime
 from pathlib import Path
 
@@ -7,8 +8,12 @@ def get_root_dir():
     return [p for p in current_dir.parents if p.parts[-1] == 'mezcal'][0]
 
 
+def get_data_dir():
+    return os.path.join(get_root_dir(), "data")
+
+
 def save_model(learner, title):
-    now = datetime.datetime.now()
+    now = datetime.now()
     timestamp = str(now.strftime("%Y%m%d_%H-%M-%S"))
 
     fname = f'{title}-{timestamp}'
