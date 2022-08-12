afcd72207147323d5f91d3e95ea10bfd590281c6
socd06 <socd06@gmail.com>
2022-08-12 16:20:29 -0400

    add feature request issue type

---
+++ scripts/aca.py
@@ -0,0 +1,3 @@
+from scripts.helpers import *
+
+print(get_root_dir())
---
+++ scripts/spcb.py
@@ -0,0 +1,8 @@
+from fasterai.distill.distillation_callback import KnowledgeDistillationCallback
+from fasterai.sparse.all import *
+from fastai.test_utils import *
+from fasterai.distill import *
+
+
+
+KnowledgeDistillationCallback(teacher, loss)
