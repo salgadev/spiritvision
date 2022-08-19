0ea14ae383e99f1e68d158456e57fbf00388a244
socd06 <socd06@gmail.com>
2022-08-18 23:15:38 -0400

    add architecture argument

--- scripts/train.py
+++ scripts/train.py
@@ -1,17 +1,24 @@
 from fastai.vision.widgets import *
 from helpers import *
+import argparse
 
 
-def main():
+def main(arguments):
+    arch = int(arguments.arch)
+
     # batch size of 9 because of small dataset
     data_loader = make_data_loader(get_data_dir(), batch_size=9)
 
-    arch = 34
     learn = resnet_learner(data_loader, arch)
     learn.fine_tune(10)
 
-    save_to_model_folder(learn, f"resnet{arch}_model.pth")
+    save_path = os.path.join(get_models_dir(), f"resnet{arch}_model")
+    learn.save(save_path)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--arch', required=True, help="ResNet architecture. Choose from 18, 34 or 50")
+
+    args = parser.parse_args()
+    main(args)
