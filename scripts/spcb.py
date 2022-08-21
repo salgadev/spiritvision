197e8a58c261026f66dce7ade0254ffe14657946
socd06 <socd06@gmail.com>
2022-08-20 22:59:05 -0400

    remove unused file

--- scripts/spcb.py
+++
@@ -1,8 +0,0 @@
-from fasterai.distill.distillation_callback import KnowledgeDistillationCallback
-from fasterai.sparse.all import *
-from fastai.test_utils import *
-from fasterai.distill import *
-
-
-
-KnowledgeDistillationCallback(teacher, loss)
