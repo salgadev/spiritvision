9612e6dec737bfd5e1d8e64d9202891df1cd0453
socd06 <socd06@gmail.com>
2022-08-26 22:36:45 -0400

    add

---
+++ scripts/optimize.py
@@ -0,0 +1,53 @@
+# all imports
+
+from helpers import *
+import argparse
+
+import sys
+import os
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('..\\scripts'))
+
+from fastai.vision.widgets import *
+from fastbook import *
+from helpers import *
+
+import torch
+import torchvision
+from torch.utils.mobile_optimizer import optimize_for_mobile
+
+def main(arguments):
+    arch = int(arguments.arch)
+
+
+    data_loader = make_data_loader(get_data_dir(), batch_size = 9)
+
+    arch = 18
+    learn = resnet_learner(data_loader, arch)
+
+    load_path = os.path.join(get_models_dir(), f"resnet{arch}_model")
+    learn.load(load_path)
+
+    quantized = nn.Sequential(learn.model,nn.Softmax()).to('cpu')
+    quantized.eval()
+
+    model_int8 = torch.quantization.quantize_dynamic(quantized,
+                                                    {torch.nn.Sequential},
+                                                    dtype=torch.qint8)
+
+    dummy_input = torch.rand(1, 3, 224, 224)
+    traced_script_module = torch.jit.trace(model_int8, dummy_input)
+
+    original_model_name = load_path.split("\\")[-1]
+    optimized_model_path = os.path.join(get_models_dir(), f"optimized_torchscript_{original_model_name}.ptl")
+
+    traced_script_module_optimized = optimize_for_mobile(traced_script_module)
+    traced_script_module_optimized._save_for_lite_interpreter(optimized_model_path)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--arch', required=True, help="ResNet architecture. Choose from 18, 34 or 50")
+
+    args = parser.parse_args()
+    main(args)
