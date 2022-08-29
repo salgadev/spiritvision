b8aa9a20e6fdd0377b53a287dd3188814742aa63
socd06 <socd06@gmail.com>
2022-08-28 23:02:46 -0400

    add get_folder_and_filename, add docstrings

--- scripts/crop.py
+++ scripts/crop.py
@@ -7,9 +7,26 @@ processed_path = get_processed_data_dir()
 data_path = get_raw_data_dir()
 
 
-def crop_background(path):
+def get_folder_and_filename(path):
+    """Splits image path into its filename and its parent folder before processing
+    :param: path: must be the path to an image
+    :return: filename and the path to parent folder
+    """
     target_folder = path.split("\\")[-2]
+    if not os.path.isdir(target_folder):
+        raise TypeError(f"{target_folder} is not a valid folder.")
+
     fname = path.split("\\")[-1]
+    f, extension = fname.lower().split(".")
+    if extension not in ["png", "bmp", "jpg", "jpeg"]:
+        raise TypeError(f"{fname} has a non-image extension: {extension}.")
+
+    return fname, target_folder
+
+
+def crop_background(path, debug=False):
+    # TODO: if debug is True should not delete ostu.png, otherwise delete it always
+    filename, target_folder = get_folder_and_filename(path)
     img = cv2.imread(path)
     grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     ret, thresholded = cv2.threshold(grayscale, 0, 255, cv2.THRESH_OTSU)
@@ -21,7 +38,7 @@ def crop_background(path):
     if not os.path.isdir(label_folder):
         os.mkdir(label_folder)
 
-    target_path = f"{label_folder}\\cropped-{fname}"
+    target_path = f"{label_folder}\\cropped-{filename}"
     if not os.path.isfile(target_path):
         print(f"Saving {target_path}")
         cv2.imwrite(target_path, foreground)
