25e37987ec202ca61510ef187f31ad47dbe3f323
socd06 <socd06@gmail.com>
2023-01-16 11:59:46 -0500

    refactor ClassificationInterpretation class for redundancy

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -1,4 +1,3 @@
-import itertools
 import os
 from datetime import datetime
 from pathlib import Path
@@ -14,59 +13,6 @@ from torchvision import transforms
 
 # probably should stay like this
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-
-class ClassificationInterpretation:
-    def __init__(self, model, dataloader):
-        self.model = model
-        self.dataloader = dataloader
-        self.class_names = dataloader.dataset.classes
-        self.num_classes = len(self.class_names)
-
-    def generate_confusion_matrix(self):
-        self.model.eval()
-        y_true = []
-        y_pred = []
-
-        with torch.no_grad():
-            for inputs, labels in self.dataloader:
-                inputs, labels = inputs.to(device), labels.to(device)
-                outputs = self.model(inputs)
-                _, preds = torch.max(outputs, 1)
-                y_true.append(labels.cpu().numpy())
-                y_pred.append(preds.cpu().numpy())
-
-        y_true = np.concatenate(y_true)
-        y_pred = np.concatenate(y_pred)
-        self.confusion_matrix = confusion_matrix(y_true, y_pred)
-
-    def plot_confusion_matrix(self, normalize=False):
-        if not hasattr(self, 'confusion_matrix'):
-            self.generate_confusion_matrix()
-
-        if normalize:
-            self.confusion_matrix = self.confusion_matrix.astype('float') / self.confusion_matrix.sum(axis=1)[:,
-                                                                            np.newaxis]
-
-        plt.figure(figsize=(10, 10))
-        plt.imshow(self.confusion_matrix, interpolation='nearest', cmap=plt.cm.Blues)
-        plt.title("Confusion matrix")
-        plt.colorbar()
-        tick_marks = np.arange(len(self.class_names))
-        plt.xticks(tick_marks, self.class_names, rotation=45)
-        plt.yticks(tick_marks, self.class_names)
-
-        fmt = '.2f' if normalize else 'd'
-        thresh = self.confusion_matrix.max() / 2.
-        for i, j in itertools.product(range(self.confusion_matrix.shape[0]), range(self.confusion_matrix.shape[1])):
-            plt.text(j, i, format(self.confusion_matrix[i, j], fmt),
-                     horizontalalignment="center",
-                     color="white" if self.confusion_matrix[i, j] > thresh else "black")
-
-        plt.tight_layout()
-        plt.ylabel('True label')
-        plt.xlabel('Predicted label')
-        plt.show()
 
 
 def get_root_dir():
@@ -121,16 +67,48 @@ def make_data_loader(data_path, batch_size):
     return data_loader
 
 
-def save_confusion_matrix_plot(confusion_matrix, labels, path):
-    disp = ConfusionMatrixDisplay(confusion_matrix=confusion_matrix, display_labels=labels)
-    disp.plot(cmap=plt.cm.Blues, values_format='g')
+class ClassificationInterpretation:
+    def __init__(self, model, dataloader):
+        self.confusion_matrix = None
+        self.model = model
+        self.dataloader = dataloader
+        self.class_names = dataloader.dataset.classes
+        self.num_classes = len(self.class_names)
+        self.generate_confusion_matrix()
 
-    plt.title("Confusion Matrix")
-    plt.xticks(rotation=90)
-    plt.tight_layout()
-    plt.ylabel('Actual')
-    plt.xlabel('Predicted')
-    plt.grid(False)
+    def generate_confusion_matrix(self):
+        self.model.eval()
+        y_true = []
+        y_pred = []
 
-    plt.imshow(confusion_matrix, interpolation='nearest', cmap="Blues")
-    plt.savefig(path, dpi=120)
+        with torch.no_grad():
+            for inputs, labels in self.dataloader:
+                inputs, labels = inputs.to(device), labels.to(device)
+                outputs = self.model(inputs)
+                _, preds = torch.max(outputs, 1)
+                y_true.append(labels.cpu().numpy())
+                y_pred.append(preds.cpu().numpy())
+
+        y_true = np.concatenate(y_true)
+        y_pred = np.concatenate(y_pred)
+        self.confusion_matrix = confusion_matrix(y_true, y_pred)
+
+    def save_confusion_matrix_plot(self, path):
+        # TODO: Use seaborn or plotly
+        cm = self.confusion_matrix
+        disp = ConfusionMatrixDisplay(
+            confusion_matrix=cm,
+            display_labels=self.class_names)
+        disp.plot(cmap=plt.cm.Blues, values_format='g')
+
+        plt.title("Confusion Matrix")
+        plt.xticks(rotation=90)
+        plt.tight_layout()
+        plt.ylabel('Actual')
+        plt.xlabel('Predicted')
+        plt.grid(False)
+
+        plt.imshow(cm, interpolation='nearest', cmap="Blues")
+        saving_path = path.replace('.pth', '.PNG')
+        plt.savefig(saving_path, dpi=120)
+        print(f'Saved {saving_path} successfully!')
