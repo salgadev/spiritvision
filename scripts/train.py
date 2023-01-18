d03cfa2d2469e1024ee7447624dd459f4da174e0
socd06 <socd06@gmail.com>
2023-01-18 00:22:11 -0500

    optimize imports, parse arguments inside main function

--- scripts/train.py
+++ scripts/train.py
@@ -1,10 +1,16 @@
-from fastai.vision.widgets import *
+from fastai.interpret import ClassificationInterpretation
 from helpers import *
 import argparse
 
 
-def main(arguments):
-    arch = int(arguments.arch)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--arch',
+                        required=True,
+                        help="ResNet architecture. Choose from 18, 34 or 50")
+
+    args = parser.parse_args()
+    arch = int(args.arch)
 
     # batch size of 9 because of small dataset
     data_loader = make_data_loader(get_processed_data_dir(), batch_size=9)
@@ -12,19 +18,22 @@ def main(arguments):
     learn = resnet_learner(data_loader, arch)
     learn.fine_tune(10)
 
-    save_path = os.path.join(get_models_dir(), f"resnet{arch}_model")
+    now = datetime.now()
+    timestamp = str(now.strftime("%Y%h%d_%I%M%p"))
+
+    save_path = os.path.join(get_models_dir(),
+                             f"resnet_{arch}_{timestamp}")
     learn.save(save_path)
 
     interp = ClassificationInterpretation.from_learner(learn)
     cm = interp.confusion_matrix()
-    cm_png_path = os.path.join(get_root_dir(), f"confusion_matrix_ResNet{arch}.png")
 
-    save_confusion_matrix_plot(confusion_matrix=cm, labels=data_loader.vocab, path=cm_png_path)
+    cm_png_path = f"{save_path}_confusion_matrix.PNG"
+
+    save_confusion_matrix_plot(confusion_matrix=cm,
+                               labels=data_loader.vocab,
+                               path=cm_png_path)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--arch', required=True, help="ResNet architecture. Choose from 18, 34 or 50")
-
-    args = parser.parse_args()
-    main(args)
+    main()
